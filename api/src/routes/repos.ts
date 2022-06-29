1cb05b42d666015eba525a9db1feecd98e66e586
Kyrill Metalnikov <kmetalnikov@hotmail.com>
2022-06-29 13:18:12 -0700

    Added the JSON file data to the /repos endpoint

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response } from 'express';
 import { Repo } from '../models/Repo';
+import reposFileData from '../../data/repos.json'
 const XMLHttpRequest = require('xhr2');
 
 export const repos = Router();
 
 const repoURL = "https://api.github.com/users/silverorange/repos";
 
+// Gets the repo data from the provided url
 const fetchRepoData = () => new Promise<Array<Repo>>(resolve => {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', repoURL);
@@ -15,6 +17,7 @@ const fetchRepoData = () => new Promise<Array<Repo>>(resolve => {
   xhr.send();
 });
 
+// Filters the repos to only keep repos where repository.fork === false
 function filterRepo(repository: Array<Repo>) {
   let result = [];
   for (let i = 0; i < repository.length; i++) {
@@ -30,8 +33,8 @@ repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   fetchRepoData().then((response: Array<Repo>) => {
-    let jsonResponse = response;
-    jsonResponse = filterRepo(jsonResponse);
+    let jsonResponse = filterRepo(response);
+    jsonResponse = jsonResponse.concat(filterRepo(reposFileData));
 
     res.status(200);
     res.json(jsonResponse);
