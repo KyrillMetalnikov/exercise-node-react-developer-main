124266369d95cf8390563fbab258cfdde1d49aee
Kyrill Metalnikov <kmetalnikov@hotmail.com>
2022-06-29 13:02:06 -0700

    added functionality to pull repos from the github api with basic filtering

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,39 @@
 import { Router, Request, Response } from 'express';
+import { Repo } from '../models/Repo';
+const XMLHttpRequest = require('xhr2');
 
 export const repos = Router();
+
+const repoURL = "https://api.github.com/users/silverorange/repos";
+
+const fetchRepoData = () => new Promise<Array<Repo>>(resolve => {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', repoURL);
+  xhr.onload = () => {
+      resolve(JSON.parse(xhr.responseText));
+  };
+  xhr.send();
+});
+
+function filterRepo(repository: Array<Repo>) {
+  let result = [];
+  for (let i = 0; i < repository.length; i++) {
+    if (repository[i].fork === false) {
+      result.push(repository[i]);
+    }
+  }
+
+  return result;
+}
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
-  res.status(200);
+  fetchRepoData().then((response: Array<Repo>) => {
+    let jsonResponse = response;
+    jsonResponse = filterRepo(jsonResponse);
 
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+    res.status(200);
+    res.json(jsonResponse);
+  })
 });
